98736a49a766f840c8a92c6146961f9fdd6ac102
anna-koenig <anna-koenig@web.de>
2025-03-10 12:47:54 +0100

    add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,32 @@ export default function App() {
     // define all changeable states
     const [characters, setCharacters] =  useState<Character[]>([]);
 
+    const [currentPage, setPage] =  useState<number>(1);
+
+    function nextPage() {
+        if (currentPage >= 42) {
+            setPage(42);
+        }
+        else {
+            setPage(currentPage + 1);
+        }
+
+    }
+
+    function previousPage() {
+        if (currentPage <= 1) {
+            setPage(1);
+        }
+        else {
+            setPage(currentPage - 1);
+        }
+
+    }
+    console.log("CUrrent Page " + currentPage.toString())
+
     useEffect(() => {
         loadCharacters();
-    }, []);
+    }, [currentPage]);
 
     //define function which updates the character list
     const addCharacter = (characterToAdd: Character) => {
@@ -29,7 +52,7 @@ export default function App() {
     const loadCharacters = () => {
         console.log("Loading characters from API");
 
-        axios.get("https://rickandmortyapi.com/api/character")
+        axios.get("https://rickandmortyapi.com/api/character" + "?page=" + currentPage.toString())
             .then((response) => {
                 setCharacters(response.data.results)
             })
@@ -43,7 +66,7 @@ export default function App() {
             <Header/>
             <Routes>
                 <Route path="/home/welcome" element={<Welcome/>}/>
-                <Route path="/characters" element={<CharacterGallery characters={characters}/>}/>
+                <Route path="/characters" element={<CharacterGallery characters={characters} nextPageFunction={nextPage} previousPageFunction={previousPage}/>}/>
                 <Route path="/characters/add" element={<AddCharacter addCharacter={addCharacter}/>} />
                 <Route path="/characters/:id" element={<CharacterDetails characters={characters}/>}/>
             </Routes>
--- src/components/CharacterGallery.tsx
+++ src/components/CharacterGallery.tsx
@@ -2,19 +2,24 @@ import {Character} from "../types/RickAndMortyCharacter.ts";
 import CharacterCard from "./CharacterCard.tsx";
 import "./CharacterGallery.css";
 import {useState} from "react";
+import axios from "axios";
 
 type CharacterGalleryProps = {
     characters: Character[];
+    nextPageFunction: () => void
+    previousPageFunction: () => void
 }
 export default function CharacterGallery(props: Readonly<CharacterGalleryProps>) {
 
-    const cards = props.characters.map((character) => <CharacterCard key={character.name} character={character}/>);
-
-
+    const cards = props.characters.map((character) => <CharacterCard key={character.id} character={character}/>);
 
     return (
+        <div>
         <div className="character-gallery">
             {cards}
+        </div>
+            <button onClick={props.previousPageFunction}> Previous Page</button>
+            <button onClick={props.nextPageFunction}> Next Page</button>
         </div>
     );
 }
