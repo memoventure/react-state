90ba6b6e12efb787ef6812bd5060e04f211625b7
anna-koenig <anna-koenig@web.de>
2025-03-10 11:26:14 +0100

    use axios for getting characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import CharacterGallery from "./components/CharacterGallery.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {characters} from "./AllCharacters.ts";
 import {Route, Routes} from 'react-router-dom';
 import Welcome from "./components/Welcome.tsx";
@@ -9,15 +9,33 @@ import CharacterDetails from "./components/CharacterDetails.tsx";
 import AddCharacter from "./components/AddCharacter.tsx";
 import {Character} from "./types/RickAndMortyCharacter.ts";
 import {allCharacters} from "./AllCharacters.ts";
+import axios from "axios";
 
 export default function App() {
 
     // define all changeable states
-    const [characters, setCharacters] =  useState<Character[]>(allCharacters);
+    const [characters, setCharacters] =  useState<Character[]>([]);
+
+    useEffect(() => {
+        loadCharacters();
+    }, []);
 
     //define function which updates the character list
     const addCharacter = (characterToAdd: Character) => {
+        axios.post("https://rickandmortyapi.com/api/character", characterToAdd)
         setCharacters([...characters, characterToAdd])
+    }
+
+    const loadCharacters = () => {
+        console.log("Loading characters from API");
+
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response) => {
+                setCharacters(response.data.results)
+            })
+            .catch((errorResponse) => {
+                console.log("Get request failed")
+            })
     }
 
     return (
